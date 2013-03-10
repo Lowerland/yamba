606b59363665ac194eb0d5dc384a307c9ffb6c65
Lowerland <Lowerland@hotmail.com>
2013-03-10 23:30:28 +0100

    Added a StatusProvider so it can be used by other apps. And Refactored alot of code to make it more centralized.

--- src/com/example/yamba/StatusData.java
+++ src/com/example/yamba/StatusData.java
@@ -4,10 +4,7 @@ import winterwell.jtwitter.Twitter.Status;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
-import android.util.Log;
 
 public class StatusData {
 	static final String TAG = "StatusData";
@@ -20,67 +17,25 @@ public class StatusData {
 	public static final String C_TEXT = "status_text";
 
 	Context context;
-	DbHelper dbHelper;
-	SQLiteDatabase db;
-	
+
 	public StatusData(Context context) {
 		this.context = context;
-		dbHelper = new DbHelper();
 	}
-	
+
 	public void insert(Status status) {
-		
+
 		ContentValues values = new ContentValues();
 		values.put(C_ID, status.id);
 		values.put(C_CREATED_AT, status.createdAt.getTime());
 		values.put(C_USER, status.user.name);
 		values.put(C_TEXT, status.text);
-		
-		
-		db = dbHelper.getWritableDatabase(); 
-		db.insertWithOnConflict(TABLE, null, values, SQLiteDatabase.CONFLICT_IGNORE);		
-		
-		
+
+		context.getContentResolver().insert(StatusProvider.CONTENT_URI, values);
+
 	}
-	
+
 	public Cursor query() {
-		db = dbHelper.getReadableDatabase();
-		Cursor cursor = db.query(TABLE, null, null, null, null, null, C_CREATED_AT + " DESC");
-		
-		return cursor;
-		
+		return context.getContentResolver().query(StatusProvider.CONTENT_URI,
+				null, null, null, C_CREATED_AT + " DESC");
 	}
-	
-
-	
-	class DbHelper extends SQLiteOpenHelper {
-
-		public DbHelper() {
-			super(context, DB_NAME, null, DB_VERSION);
-
-		}
-
-		@Override
-		public void onCreate(SQLiteDatabase db) {
-			String sql = String.format("create table %s" + 
-					"(%s int primary key, %s int, %s text, %s text)",
-					TABLE, C_ID, C_CREATED_AT, C_USER, C_TEXT);
-			Log.d(TAG, "onCreate with SQL: "+sql);
-
-			db.execSQL(sql);
-
-		}
-
-		@Override
-		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-			Log.d(TAG, "onUpgrade from "+oldVersion+" to "+newVersion);
-			// Usually ALTER TABLE statement
-			db.execSQL("drop table if exists"+TABLE);
-			onCreate(db);
-			
-			
-		}
-
-	}
-
 }
